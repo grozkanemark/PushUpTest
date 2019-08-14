e78fa26ec9ad5d4f07d6c06e747da25405d57588
Marcin Kucharczyk <grozkanemark@gmail.com>
2019-08-14 11:21:53 +0200

    Initial commit

---
+++ app/src/main/java/com/example/pushuptest/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.pushuptest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void pushUpTest() {
+        int i = 1 +1;
+    }
+
+}
+
